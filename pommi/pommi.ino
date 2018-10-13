e854519e117df27e59f1b5a4b8eacd46aa117897
aajarven <anni.jarvenpaa@gmail.com>
2018-10-14 01:34:01 +0300

    automatic neat(ish) printing for long strings on display

--- pommi/pommi.ino
+++ pommi/pommi.ino
@@ -4,7 +4,6 @@
 #define N_ANALOG_PINS 6
 #define ANALOG_TOLERANCE 10
 
-
 int seed = 7155;
 int n_functions = 7;
 
@@ -18,6 +17,12 @@ int button1_pin = 5;
 // temporary pins
 int *switch_pins[] = {10, 11, 12, 13};
 
+int screen_width = 20; // columns
+int screen_height = 4; // rows
+char hyphenation_hint = '|';
+
+int buttondelay = 400; // delay length after button press to prevent doubleclick accidents
+
 typedef bool (*module_function)();
 module_function functions[] = {&potentiometer_puzzle,
                                &switch_puzzle,
@@ -30,7 +35,21 @@ module_function functions[] = {&potentiometer_puzzle,
 
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
+struct TextRow {
+  /**
+   * Used to store instructions and other text splitted to individual rows.
+   * Resembles a node in a linked list. Contains a pointer to the text row.
+   * For the first and last rows the previous_row and next_row (respectively)
+   * are NULL.
+   */
+   
+    TextRow* previous;
+    TextRow* next;
+    char* text;
+};
+
 void setup() {
+  
   randomSeed(seed);
   for (int pin = 8; pin > 1; pin--) {
     pinMode(pin, OUTPUT);
@@ -52,6 +71,8 @@ void loop() {
   //  for (int i = 0; i < n_functions; i++) {
   //    functions[i](seed);
   //  }
+  
+  display_message("tosi|pitka|sana|tatapas|et|saa|naytolle|kerralla testi testi tassaeioletavutusvihjeitamahtaavituttaa testi testi testi testi asdf fdsa");
 
   read_digital_values(digital_pin_values);
   read_analog_values(analog_pin_values);
@@ -250,5 +271,156 @@ void read_analog_values(int pin_values[N_ANALOG_PINS]) {
   for (int i = 0; i < N_ANALOG_PINS; i++) {
     pin_values[i] = analogRead(i);
   }
+}
+
+struct TextRow* split_message(String message, int row_length) {
+  /**
+   * Splits the given string to pieces with a maximum length of the given row_length.
+   * Splits are allowed at whitespace characters and dashes and additional positions
+   * where hyphenation is allowed can be specified by inserting a hyphenation hint
+   * (see variable hyphenation_hint) into the text. If a word is longer than the row,
+   * a row is filled and a new line added.
+   * 
+   * Returns the first row of text as an TextRow.
+   */
+
+   struct TextRow* first_row = new TextRow;
+   (*first_row).previous = NULL;
+   (*first_row).next = NULL;
+   (*first_row).text = NULL;
+
+   int char_index = 0;
+   struct TextRow* row = first_row;
+   
+   while (char_index < message.length()) {
+    char* text = malloc(sizeof(char) * (row_length + 1));
+    int last_break = 0;
+    int row_index = 0;
+    bool requires_hyphenation = true;
+    
+    while (row_index < row_length){
+      char c = message.charAt(char_index);
+      
+      if (c == '\n') {
+        text[row_index] = '\0';
+        char_index++;
+        last_break = row_index;
+        requires_hyphenation = false;
+        break;
+        
+      } else if (c == '\0') {
+        text[row_index] = c;
+        requires_hyphenation = false;
+        last_break = row_index;
+        char_index++;
+        break;
+        
+      } else if (c == ' ') {
+        if (row_index != 0) {
+          text[row_index] = c;
+          last_break = row_index;
+          row_index++;
+          requires_hyphenation = false;
+        }
+        char_index++;
+        
+      } else if (c == hyphenation_hint) {
+        char_index++;
+        last_break = row_index;
+        requires_hyphenation = true;
+        
+      } else {
+        text[row_index] = c;
+        char_index++;
+        row_index++;
+      }
+    }
+
+    /* ensure that the row is properly terminated at an allowed position (if possible),
+     * add hyphenation (if needed) and set the char index so that the next row starts
+     * after the last allowed line terminator
+     */
+    if (last_break == 0) {
+      text[row_length-1] = '-';
+      text[row_length] = '\0';
+      char_index--;
+    } else {
+      if (requires_hyphenation) {
+        text[last_break] = '-';
+      }
+      text[last_break + 1] = '\0';
+      char_index -= (row_index - last_break);
+    }
+
+    (*row).text = text;
+
+    if (char_index < message.length()){
+      struct TextRow* next = new TextRow;
+      (*next).previous = row;
+      (*next).next = NULL;
+      (*row).next = next;
+      row = next;
+    }
+   }
+
+   return first_row;
+}
+
+void display_message(String message) {
+  struct TextRow *first_row = split_message(message, screen_width);
+
+  // count rows
+  int rows = 0;
+  struct TextRow *message_row = first_row;
+  while((*message_row).next) {
+    rows++;
+    message_row = (*message_row).next;
+  }
+
+  // show the text
+  int top_row_index = 0;
+  struct TextRow *top_row = first_row;
+  bool refresh = true;
+  while (top_row_index <= rows){
+    struct TextRow *printed = top_row;
+    if (refresh){
+      lcd.clear();
+      for (int i=0; i<screen_height; i++){
+          if (top_row_index + i <= rows) {
+            lcd.setCursor(0, i);
+            lcd.print((*printed).text);
+            printed = (*printed).next;
+          }
+      }
+      refresh = false;
+    }
+
+    // move forward?
+    if (digitalRead(button1_pin) == HIGH) {
+      while(digitalRead(button1_pin) == HIGH){
+        delay(buttondelay);
+      }
+      top_row = (*top_row).next;
+      top_row_index++;
+      refresh = true;
+      delay(buttondelay);
+    }
+  }
+
+  // free the memory
+  freeTexts(first_row);
+  
+}
+
+void freeTexts(struct TextRow *message){
+  struct TextRow *current_row = message;
+  while ((*current_row).next){
+    struct TextRow *next_row = (*current_row).next;
+    free((*current_row).text);
+    free(current_row);
+    current_row = next_row;
+  }
+  free((*current_row).text);
+  free(current_row);
 }
 
